aff66714abcdeba04ac1350daa3d101f5b8188dc
blessingmwiti <blessingmwiti@gmail.com>
2023-11-04 08:42:59 +0300

    Added app logo generated by AI

--- app/build/generated/source/buildConfig/debug/com/my/mdmd/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/my/mdmd/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.my.mdmd";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.4";
+  public static final String VERSION_NAME = "1.5";
 }
